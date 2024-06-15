3269cd4cdb924d03795b8839188940eb77664e4f
Rizza Marwan <rizzamarwan@icloud.com>
2024-06-16 00:01:21 +0700

    Update
    Added total counts

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let countEl = document.getElementById("count-value")
 let saveEl = document.getElementById("saved-value")
+let totalEl = document.getElementById("total-value")
+
 let count = 0
+let totalCount = 0
 let saveState = 1
 
 function increment() {
@@ -17,12 +20,14 @@ function decrement() {
 
 function save() {
     let countedFor = count
+    totalCount += countedFor
     let counts = (count > 1) ? "Counts" : "Count"
     saveEl.innerHTML += `
         <div>
-            <h2 id="saved-value">Save ${saveState++}: ${countedFor} ${counts}</h2>
+            <p>Save ${saveState++}: ${countedFor} ${counts}</p>
         </div>
     `
     count = 0
     countEl.textContent = count
+    totalEl.textContent = "Total: " + totalCount + " Counted so far "
 }
